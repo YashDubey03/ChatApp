08b2a859bb9d01432b32df3ab4b7a79eb4248178
Yash <yashvardhandubey83@gmail.com>
2026-01-26 23:48:24 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,12 @@ const PORT = process.env.PORT || 5000
 
 // connect to db
 await connectDB()
-server.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`)
-})
+
+if (process.env.NODE_ENV !== "production") {
+
+    server.listen(PORT, () => {
+        console.log(`Server running on PORT ${PORT}`)
+    })
+}
+// export server for vercel
+export default server
