642b7f24bf64ad424ed7e2aff21370534c0fc6dd
Yash <yashvardhandubey83@gmail.com>
2026-01-27 22:31:39 +0530

    corrected connection for production

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,36 @@ const app = express()
 const server = http.createServer(app)
 
 // middlewares
-app.use(express.json({ limit: "4mb" }))
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+const allowedOrigins = [
+
+    "https://chat-app-frontend-seven-ashy.vercel.app"
+];
+
+// middlewares
+app.use(express.json({ limit: "4mb" }));
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
 
 // socket.io setup
 export const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
-        credentials: true
-    }
-})
+        origin: allowedOrigins,
+        credentials: true,
+    },
+});
+
 
 // store online user 
 export const userSocketMap = {} // { userId: socketId }
